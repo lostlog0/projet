3453ec505ae94fbab4530336cca78e3ebe278672
lostlog0 <113916329+lostlog0@users.noreply.github.com>
2022-12-08 14:59:27 +0100

    Update fenetre.cpp

--- Serveur/fenetre.cpp
+++ Serveur/fenetre.cpp
@@ -13,7 +13,7 @@ MaFenetre::MaFenetre() : QWidget()
 void MaFenetre::ouvrirDialogue(string valeur, string ip_recu)
 {
 
-    QString fichier = "/home/ubuntu/nouveauprojetmaiscettefoiscavamarcher/monFichier.txt";
+    QString fichier = "/home/ubuntu/mafenetre/logs.txt";
 
     QFile file(fichier); // Appel du constructeur de la classe QFile
 
@@ -32,6 +32,6 @@ void MaFenetre::ouvrirDialogue(string valeur, string ip_recu)
 }
 void MaFenetre::slotOpenUrl() {
 
-    QDesktopServices::openUrl(QUrl("/home/ubuntu/nouveauprojetmaiscettefoiscavamarcher/monFichier.txt"));
+    QDesktopServices::openUrl(QUrl("/home/ubuntu/mafenetre/logs.txt"));
 
 }
